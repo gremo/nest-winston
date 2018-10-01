61e5ad990f46f6a4be19d2dbbc6123a866de76ca
Marco Polichetti <gremo1982@gmail.com>
2018-10-01 23:18:45 +0200

    Introduce interface for module options

--- src/winston.providers.ts
+++ src/winston.providers.ts
@@ -1,9 +1,10 @@
 import { Provider } from '@nestjs/common';
-import { createLogger, LoggerOptions } from 'winston';
+import { createLogger } from 'winston';
 
 import { WINSTON_MODULE_PROVIDER } from './winston.constants';
+import { WinstonModuleOptions } from './winston.interfaces';
 
-export function createWinstonProviders(options: LoggerOptions): Provider[] {
+export function createWinstonProviders(options: WinstonModuleOptions): Provider[] {
   return [
     {
       provide: WINSTON_MODULE_PROVIDER,
