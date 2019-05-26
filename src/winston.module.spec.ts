2d890e3a03c42ae0d0e3c69274174de2b605def8
Marco Polichetti <gremo1982@gmail.com>
2019-05-26 20:43:01 +0200

    Add some basic tests 🎉

---
+++ src/winston.module.spec.ts
@@ -0,0 +1,47 @@
+/* tslint:disable:only-arrow-functions max-classes-per-file */
+
+import { Injectable, Module } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { expect } from 'chai';
+import { WINSTON_MODULE_PROVIDER } from './winston.constants';
+import { WinstonModule } from './winston.module';
+
+describe('Winston module', function() {
+  it('boots successfully', async function() {
+    const rootModule = await Test.createTestingModule({
+      imports: [
+        WinstonModule.forRoot({}),
+      ],
+    }).compile();
+
+    expect(rootModule.get(WINSTON_MODULE_PROVIDER)).to.be.an('object');
+  });
+
+  it('boots successfully asynchronously', async function() {
+    @Injectable()
+    class ConfigService {
+      public loggerOptions = {};
+    }
+
+    @Module({
+      providers: [ConfigService],
+      exports: [ConfigService],
+    })
+    class FeatureModule {}
+
+    const rootModule = await Test.createTestingModule({
+      imports: [
+        WinstonModule.forRootAsync({
+          imports: [FeatureModule],
+          useFactory: (cfg: ConfigService) => cfg.loggerOptions,
+          inject: [ConfigService],
+        }),
+      ],
+    }).compile();
+
+    const app = rootModule.createNestApplication();
+    await app.init();
+
+    expect(rootModule.get(WINSTON_MODULE_PROVIDER)).to.be.an('object');
+  });
+});
