9137cbc10b7a14b62bd7ee5038664e54715946c5
Dan Shapir <s.dan@live.com>
2019-09-12 18:33:25 +0300

    parent 5bda4fb6cfbf8c0f36e158e3c572ce8ecdd0942e author Dan Shapir <s.dan@live.com> 1568302405 +0300 committer Dan Shapir <dan@payk.com.au> 1568309661 +0300
    Add a provider to inject
    
    Add NestWinson Contant
    
    Bump Version
    
    Update README.md
    
    Add Logger import
    
    Update winston.providers.ts
    
    Update winston.providers.ts
    
    Update README.md
    
    Update winston.module.spec.ts
    
    Update README.md
    
    Update winston.module.spec.ts
    
    Add a provider to inject
    
    Add NestWinson Contant
    
    Bump Version
    
    Update winston.providers.ts
    
    Update winston.providers.ts
    
    Update README.md
    
    Update winston.module.spec.ts
    
    Update README.md
    
    Update winston.module.spec.ts
    
    Fix merge bug

--- src/winston.providers.ts
+++ src/winston.providers.ts
@@ -1,7 +1,27 @@
-import { Provider } from '@nestjs/common';
-import { createLogger, LoggerOptions } from 'winston';
-import { WINSTON_MODULE_OPTIONS, WINSTON_MODULE_PROVIDER } from './winston.constants';
+import { Provider, LoggerService } from '@nestjs/common';
+import { createLogger, LoggerOptions, Logger } from 'winston';
+import { WINSTON_MODULE_OPTIONS, WINSTON_MODULE_PROVIDER, WINSTON_MODULE_NEST_PROVIDER } from './winston.constants';
 import { WinstonModuleAsyncOptions, WinstonModuleOptions } from './winston.interfaces';
+
+class WinstonLogger implements LoggerService {
+  constructor(private readonly logger: Logger) { }
+
+  log(message: any, context?: string) {
+    return this.logger.info(message, context);
+  }
+  error(message: any, trace?: string, context?: string) {
+    return this.logger.error(message, trace, context);
+  }
+  warn(message: any, context?: string) {
+    return this.logger.warn(message, context);
+  }
+  debug?(message: any, context?: string) {
+    return this.logger.debug(message, context);
+  }
+  verbose?(message: any, context?: string) {
+    return this.logger.verbose(message, context);
+  }
+}
 
 export function createWinstonProviders(loggerOpts: WinstonModuleOptions): Provider[] {
   return [
@@ -9,6 +29,13 @@ export function createWinstonProviders(loggerOpts: WinstonModuleOptions): Provid
       provide: WINSTON_MODULE_PROVIDER,
       useFactory: () => createLogger(loggerOpts),
     },
+    {
+      provide: WINSTON_MODULE_NEST_PROVIDER,
+      useFactory: (logger: Logger) => {
+        return new WinstonLogger(logger);
+      },
+      inject: [WINSTON_MODULE_PROVIDER],
+    }
   ];
 }
 
@@ -23,6 +50,13 @@ export function createWinstonAsyncProviders(options: WinstonModuleAsyncOptions):
       provide: WINSTON_MODULE_PROVIDER,
       useFactory: (loggerOpts: LoggerOptions) => createLogger(loggerOpts),
       inject: [WINSTON_MODULE_OPTIONS],
+    },
+    {
+      provide: WINSTON_MODULE_NEST_PROVIDER,
+      useFactory: (logger: Logger) => {
+        return new WinstonLogger(logger);
+      },
+      inject: [WINSTON_MODULE_PROVIDER],
     },
   ];
 }
