4ca6fa41ec53339705b441c6d9a1f760eb6b39a2
Marco Polichetti <gremo1982@gmail.com>
2019-09-17 20:19:37 +0200

    Pass the extra data as a meta object in Nest logger
    The context and trace are ignored when formatted by winston if not passed as objects.

--- src/winston.providers.ts
+++ src/winston.providers.ts
@@ -7,23 +7,23 @@ class WinstonLogger implements LoggerService {
   constructor(private readonly logger: Logger) { }
 
   public log(message: any, context?: string) {
-    return this.logger.info(message, context);
+    return this.logger.info(message, { context });
   }
 
   public error(message: any, trace?: string, context?: string): any {
-    return this.logger.error(message, trace, context);
+    return this.logger.error(message, { trace, context });
   }
 
   public warn(message: any, context?: string): any {
-    return this.logger.warn(message, context);
+    return this.logger.warn(message, { context });
   }
 
   public debug?(message: any, context?: string): any {
-    return this.logger.debug(message, context);
+    return this.logger.debug(message, { context });
   }
 
   public verbose?(message: any, context?: string): any {
-    return this.logger.verbose(message, context);
+    return this.logger.verbose(message, { context });
   }
 }
 
