17216887f33def9fed6918327a26945cb734f794
Marco Polichetti <gremo1982@gmail.com>
2019-12-03 19:19:20 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './winston.constants';
+export * from './winston.interfaces';
 export * from './winston.module';
 export * from './winston.utilities';
-export * from './winston.interfaces';
