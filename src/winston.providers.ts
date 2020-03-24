bcc845eab29a14c7472b5fddd58bf5b3bddcc606
ivan.brcko <brc_6@hotmail.com>
2020-03-24 17:33:57 +0100

    Add "context" property to WinstonLogger
    Add "context" private property and corresponding setter to WinstonLogger.
    Improve all logging methods inside WinstonLogger to use context property value if no context is passed explicitly.

--- src/winston.providers.ts
+++ src/winston.providers.ts
@@ -4,26 +4,32 @@ import { WINSTON_MODULE_NEST_PROVIDER, WINSTON_MODULE_OPTIONS, WINSTON_MODULE_PR
 import { WinstonModuleAsyncOptions, WinstonModuleOptions, WinstonModuleOptionsFactory } from './winston.interfaces';
 
 class WinstonLogger implements LoggerService {
+  private context?: string;
+
   constructor(private readonly logger: Logger) { }
 
+  public setContext(context: string) {
+    this.context = context;
+  }
+
   public log(message: any, context?: string) {
-    return this.logger.info(message, { context });
+    return this.logger.info(message, {context: context || this.context});
   }
 
   public error(message: any, trace?: string, context?: string): any {
-    return this.logger.error(message, { trace, context });
+    return this.logger.error(message, {trace, context: context || this.context});
   }
 
   public warn(message: any, context?: string): any {
-    return this.logger.warn(message, { context });
+    return this.logger.warn(message, {context: context || this.context});
   }
 
   public debug?(message: any, context?: string): any {
-    return this.logger.debug(message, { context });
+    return this.logger.debug(message, {context: context || this.context});
   }
 
   public verbose?(message: any, context?: string): any {
-    return this.logger.verbose(message, { context });
+    return this.logger.verbose(message, {context: context || this.context});
   }
 }
 
