841ac2c935786f9bebdf6c1db144ec5bed351be3
Dmytro Vorotyntsev <dmytro.vorotyntsev@gmail.com>
2020-09-10 01:04:55 +0300

    Allow metadata support without breaking Nest interface (#228)
    Allow metadata support in logger
    
    Co-authored-by: Marco Polichetti <gremo1982@gmail.com>

--- src/winston.classes.ts
+++ src/winston.classes.ts
@@ -11,23 +11,70 @@ export class WinstonLogger implements LoggerService {
     this.context = context;
   }
 
-  public log(message: any, context?: string) {
-    return this.logger.info(message, { context: context || this.context });
+  public log(message: any, context?: string): any {
+    context = context || this.context;
+
+    if('object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.info(msg as string, { context, ...meta });
+    }
+
+    return this.logger.info(message, { context });
   }
 
   public error(message: any, trace?: string, context?: string): any {
-    return this.logger.error(message, { trace, context: context || this.context });
+    context = context || this.context;
+
+    if(message instanceof Error) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { message: msg, name, stack, ...meta } = message;
+
+      return this.logger.error(msg, { context, stack: [trace || message.stack], ...meta });
+    }
+
+    if('object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.error(msg as string, { context, stack: [trace], ...meta });
+    }
+
+    return this.logger.error(message, { context, stack: [trace] });
   }
 
   public warn(message: any, context?: string): any {
-    return this.logger.warn(message, { context: context || this.context });
+    context = context || this.context;
+
+    if('object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.warn(msg as string, { context, ...meta });
+    }
+
+    return this.logger.warn(message, { context });
   }
 
   public debug?(message: any, context?: string): any {
-    return this.logger.debug(message, { context: context || this.context });
+    context = context || this.context;
+
+    if('object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.debug(msg as string, { context, ...meta });
+    }
+
+    return this.logger.debug(message, { context });
   }
 
   public verbose?(message: any, context?: string): any {
-    return this.logger.verbose(message, { context: context || this.context });
+    context = context || this.context;
+
+    if('object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.verbose(msg as string, { context, ...meta });
+    }
+
+    return this.logger.verbose(message, { context });
   }
 }
