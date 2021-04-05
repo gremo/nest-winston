50028e49c139564ce5fc904e31264cc6b2b48cdc
Marco Polichetti <gremo1982@gmail.com>
2021-04-05 11:54:51 +0200

    Respect custom timestamp format in nestLike closes #320

--- src/winston.utilities.ts
+++ src/winston.utilities.ts
@@ -13,12 +13,19 @@ const nestLikeColorScheme: Record<string, bare.Format> = {
 };
 
 const nestLikeConsoleFormat = (appName = 'NestWinston'): Format => format.printf(({ context, level, timestamp, message, ...meta }) => {
+  // Only format the timestamp to a locale representation if it's ISO 8601 format (winston.format.timestamp default format)
+  if ('undefined' !== typeof timestamp) {
+    if (timestamp === (new Date(timestamp)).toISOString()) {
+      timestamp = (new Date(timestamp)).toLocaleString();
+    }
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   const color = nestLikeColorScheme[level] || ((text: string): string => text);
 
   return `${color(`[${appName}]`)} ` +
          `${clc.yellow(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
-         ('undefined' !== typeof timestamp ? `${new Date(timestamp).toLocaleString()} ` : '') +
+         ('undefined' !== typeof timestamp ? `${timestamp} ` : '') +
          ('undefined' !== typeof context ? `${clc.yellow('[' + context + ']')} ` : '') +
          `${color(message)} - ` +
          `${safeStringify(meta)}`;
