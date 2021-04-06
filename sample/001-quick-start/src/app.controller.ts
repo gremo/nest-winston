28047be80b31215e41d523933a73ed437ae6fa01
Marco Polichetti <gremo1982@gmail.com>
2021-04-06 11:05:41 +0200

    Adds the quick start sample

---
+++ sample/001-quick-start/src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    this.logger.info('Called route getHello() of AppController');
+
+    return this.appService.getHello();
+  }
+}
