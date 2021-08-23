2ac597e8e0eb46402d66b53489e43fb946676ac6
mohsen <mohsensaremi@github.com>
2021-08-23 14:04:10 +0430

    add without color options. use this option when you want to write logs in text files

--- src/winston.utilities.ts
+++ src/winston.utilities.ts
@@ -4,6 +4,10 @@ import clc from 'cli-color';
 import { format } from 'winston';
 import safeStringify from 'fast-safe-stringify';
 
+type NestLikeConsoleFormatOptions = {
+  useColor?: boolean;
+};
+
 const nestLikeColorScheme: Record<string, bare.Format> = {
   info: clc.greenBright,
   error: clc.red,
@@ -12,7 +16,12 @@ const nestLikeColorScheme: Record<string, bare.Format> = {
   verbose: clc.cyanBright,
 };
 
-const nestLikeConsoleFormat = (appName = 'NestWinston'): Format => format.printf(({ context, level, timestamp, message, ms, ...meta }) => {
+const nestLikeConsoleFormat = (appName = 'NestWinston', options?: NestLikeConsoleFormatOptions): Format => format.printf(({ context, level, timestamp, message, ms, ...meta }) => {
+  options = {
+    useColor: true,
+    ...options
+  };
+
   if ('undefined' !== typeof timestamp) {
     // Only format the timestamp to a locale representation if it's ISO 8601 format. Any format
     // that is not a valid date string will throw, just ignore it (it will be printed as-is).
@@ -24,16 +33,18 @@ const nestLikeConsoleFormat = (appName = 'NestWinston'): Format => format.printf
     }
   }
 
+  const noColor = (text: string): string => text;
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  const color = nestLikeColorScheme[level] || ((text: string): string => text);
+  const color = options.useColor ? nestLikeColorScheme[level] || noColor : noColor;
+  const clcColor = options.useColor ? clc.yellow : noColor;
 
   return `${color(`[${appName}]`)} ` +
-         `${clc.yellow(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
+         `${clcColor(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
          ('undefined' !== typeof timestamp ? `${timestamp} ` : '') +
-         ('undefined' !== typeof context ? `${clc.yellow('[' + context + ']')} ` : '') +
+         ('undefined' !== typeof context ? `${clcColor('[' + context + ']')} ` : '') +
          `${color(message)} - ` +
          `${safeStringify(meta)}` +
-         ('undefined' !== typeof ms ? ` ${clc.yellow(ms)}` : '');
+         ('undefined' !== typeof ms ? ` ${clcColor(ms)}` : '');
 });
 
 export const utilities = {
