09a5b71adce6d0aafbb6418ae1f13c74345aa25a
mohsen <mohsensaremi@github.com>
2021-08-25 09:55:45 +0430

    add boots successfully with nestLikeConsoleFormat format test

--- src/winston.module.spec.ts
+++ src/winston.module.spec.ts
@@ -1,8 +1,10 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Injectable, Logger, Module } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { WINSTON_MODULE_NEST_PROVIDER, WINSTON_MODULE_PROVIDER } from './winston.constants';
 import { WinstonModuleOptions, WinstonModuleOptionsFactory } from './winston.interfaces';
 import { WinstonModule } from './winston.module';
+import * as winston from 'winston';
+import { utilities } from './winston.utilities';
 
 describe('Winston module', function () {
   it('boots successfully', async function () {
@@ -68,5 +70,43 @@ describe('Winston module', function () {
 
     expect(rootModule.get(WINSTON_MODULE_PROVIDER)).toBeDefined();
     expect(rootModule.get(WINSTON_MODULE_NEST_PROVIDER)).toBeDefined();
+  });
+
+  it('boots successfully with nestLikeConsoleFormat format', async function () {
+    const rootModule = await Test.createTestingModule({
+      imports: [
+        WinstonModule.forRoot({
+          transports: [
+            new winston.transports.Console({
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                utilities.format.nestLike(),
+              ),
+            }),
+            new winston.transports.Console({
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                utilities.format.nestLike('NestWinston', { useColor: false }),
+              ),
+            }),
+          ]
+        }),
+      ],
+    }).compile();
+
+    const app = rootModule.createNestApplication();
+    await app.init();
+
+    expect(rootModule.get(WINSTON_MODULE_PROVIDER)).toBeDefined();
+    expect(rootModule.get(WINSTON_MODULE_NEST_PROVIDER)).toBeDefined();
+
+    app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+    const logger = new Logger('TEST');
+    const loggerMock = jest.spyOn(logger, 'log');
+    logger.log('testing nestLike format with color and without color');
+    
+    expect(loggerMock).toHaveBeenCalled();
   });
 });
