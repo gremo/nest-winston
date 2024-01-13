a48aaa2a24a41809bd4d40f85d43d01525ab6749
Christopher Angelo <angelo@angeloanan.xyz>
2024-01-13 10:37:59 +0700

    Add fatal logging method

--- src/winston.classes.ts
+++ src/winston.classes.ts
@@ -23,6 +23,25 @@ export class WinstonLogger implements LoggerService {
     return this.logger.info(message, { context });
   }
 
+  public fatal(message: any, trace?: string, context?: string): any {
+    context = context || this.context;
+
+    if (message instanceof Error) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { message: msg, name, stack, ...meta } = message;
+
+      return this.logger.log({ level: 'fatal', message: msg, context, stack: [trace || stack], error: message, ...meta });
+    }
+
+    if (!!message && 'object' === typeof message) {
+      const { message: msg, ...meta } = message;
+
+      return this.logger.error({ level: 'fatal', message: msg, context, stack: [trace], ...meta });
+    }
+
+    return this.logger.error({ level: 'fatal', message, context, stack: [trace] });
+  }
+
   public error(message: any, trace?: string, context?: string): any {
     context = context || this.context;
 
